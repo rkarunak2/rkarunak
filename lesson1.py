9def6151927e005274afa9fe7d916d4cedf28042
rkarunak2 <rkarunak2@gmail.com>
2024-09-04 23:35:13 +0000

    Lesson 1 done

---
+++ lesson1.py
@@ -0,0 +1,22 @@
+# name = input("What's your name?")
+# print("name " + name)
+# print(f"Hello,{name}")
+
+# def greet(name):
+#     print(f"Hello, {name}.")
+
+# greet("Rama")
+# greet("Karuna")
+
+def add(a,b):
+    print(a+b)
+
+#add(1,2)
+
+num1 = int(input("number 1:"))
+num2 = int(input("number 2:"))
+
+add(num1,num2)
+
+
+
