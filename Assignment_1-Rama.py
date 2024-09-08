85e27cce230655746b3b417f4cf837692e1b93f6
rkarunak2 <rkarunak2@gmail.com>
2024-09-08 15:14:46 +0000

    new file:   Assignment_1-Rama.py

---
+++ Assignment_1-Rama.py
@@ -0,0 +1,18 @@
+# 1. Ask the user for their name and age, then greet them by name, and tell them how old they will be in 100 years. Use f-strings, not concatenation.
+
+name = input("What is your name?").capitalize()
+age = input("what is your age?")
+print(type(name))
+print(type(age))
+
+#print(f"Hello, {name}, you will be {str(age+100)} in 100 years.")
+
+
+# 2. Create a tip calculator program using functions and user input. Use a return statement in your function.
+
+# def tip_calculator(amount,tip_percent):
+#     return(round(amount+amount*tip_percent*0.01,2))
+# cost = float(input("what is the amount in the receipt? "))
+# tip = float(input("How much tip would you want to give? E.g 18,20,30 "))
+# total = tip_calculator(cost,tip)
+# print(total)
