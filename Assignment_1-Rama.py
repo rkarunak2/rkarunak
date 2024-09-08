8a97a3b80713dc2d81293177b265d82d2d121c7f
rkarunak2 <rkarunak2@gmail.com>
2024-09-08 11:24:43 -0400

    Update Assignment_1-Rama.py

--- Assignment_1-Rama.py
+++ Assignment_1-Rama.py
@@ -2,17 +2,14 @@
 
 name = input("What is your name?").capitalize()
 age = input("what is your age?")
-print(type(name))
-print(type(age))
-
-#print(f"Hello, {name}, you will be {str(age+100)} in 100 years.")
+print(f"Hello, {name}, you will be {str(age+100)} in 100 years.")
 
 
 # 2. Create a tip calculator program using functions and user input. Use a return statement in your function.
 
-# def tip_calculator(amount,tip_percent):
-#     return(round(amount+amount*tip_percent*0.01,2))
-# cost = float(input("what is the amount in the receipt? "))
-# tip = float(input("How much tip would you want to give? E.g 18,20,30 "))
-# total = tip_calculator(cost,tip)
-# print(total)
+def tip_calculator(amount,tip_percent):
+  return(round(amount+amount*tip_percent*0.01,2))
+cost = float(input("what is the amount in the receipt? "))
+tip = float(input("How much tip would you want to give? E.g 18,20,30 "))
+total = tip_calculator(cost,tip)
+print(total)
